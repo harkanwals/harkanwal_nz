bcb93bee272ec5189563471f624c1cbca74ec6db
Harkanwal <harkanwal@elementsdata.studio>
2018-09-19 12:18:36 +1200

    Add footer with contact details

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,32 +1,76 @@
 import React from "react";
 import styled from "styled-components";
+import { FaTwitter } from "react-icons/fa";
 
 const FooterContainer = styled.div`
-  display: flex;
-  flex-flow: row wrap;
+  
+
   width: 100%;
   padding: 1.45rem 1.0875rem;
-  clip-path: polygon(
-    0 calc(50% - 2vw),
-    100% 0,
-    100% 100%,
-    0 100%
-  );
-  background: gray; 
+  clip-path: polygon(0 calc(70% - 2vw), 100% 0, 100% 100%, 0 100%);
+  background: skyblue;
   opacity: 0.7;
   p {
     margin-top: 5%;
   }
+  @media(min-width: 1080px){
+    clip-path: polygon(0 calc(60% -2vw), 100% 0, 100% 100%, 0 100%);
+  }
+`;
+
+
+const ContactContainer = styled.div`
+  margin-top: 3%;
+  display: flex;
+  flex: row wrap;
+  justify-content: space-between;
+`
+
+const MailInfo = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  margin-left: 3%;
+  
+  a {
+    font-size: 1rem;
+    font-style: oblique 40deg;
+    color: black;
+    text-decoration: none;
+  }
+`;
+
+const ContactInfo = styled.div`
+  margin-right: 3%;
+  display: flex;
+  justify-content: flex-end;
+  a {
+    font-size: 1rem;
+    color: black;
+    text-decoration: none;
+  }
+`;
+
+const TwitterIcon = styled(FaTwitter)`
+  margin-left: 4px;
 `
 
 class Footer extends React.Component {
   render() {
     return (
       <FooterContainer>
-        <p>
-          Footer content goes here. Including Twitter link, contact, etc. 
-          Footer content goes here. Including Twitter link, contact, etc.
-        </p>
+        <ContactContainer>
+        <MailInfo>
+          <a href="mailto:harkanwal@elementsdata.studio">
+            harkanwal@elementsdata.studio
+          </a>
+        </MailInfo>
+        <ContactInfo>
+          <a href="https://twitter.com/kamal_hothi">
+            @kamal_hothi
+            <TwitterIcon />
+          </a>
+        </ContactInfo>
+        </ContactContainer>
       </FooterContainer>
     );
   }
--- src/components/showcase.js
+++ src/components/showcase.js
@@ -28,31 +28,46 @@ const FirstShowWrapper = styled.div`
 `;
 
 const Intro = styled.p`
+  display: flex;
+  justify-content: center;
   a {
     margin-top: 3px;
-    font-size: 1.1rem;
+    font-size: 1rem;
     color: gray;
-    text-decoration: underline;
-    text-decoration-color: black;
+    text-decoration: none;
+    
   }
 `;
 
+const ExternalLink = styled(FaExternalLinkAlt)`
+margin-left: 4px;
+`
 const Showcase = props => (
   <div>
     <ShowcaseWrapper>
       <FirstShowWrapper>
         <Img fluid={props.showOneImage.fluid} />
         <Intro>
-          <a href="https://insights.nzherald.co.nz/article/counting-crime/" target="_blank" rel="noopener noreferrer">
-            Mapping crime in New Zealand<FaExternalLinkAlt />
+          <a
+            href="https://insights.nzherald.co.nz/article/counting-crime/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Mapping crime in New Zealand
+            <ExternalLink />
           </a>
         </Intro>
       </FirstShowWrapper>
       <FirstShowWrapper>
         <Img fluid={props.showTwoImage.fluid} />
         <Intro>
-          <a href="https://insights.nzherald.co.nz/article/every-test-the-all-blacks-have-ever-played/" target="_blank" rel="noopener noreferrer">
-            All the All Black tests<FaExternalLinkAlt />
+          <a
+            href="https://insights.nzherald.co.nz/article/every-test-the-all-blacks-have-ever-played/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            All the All Black tests
+            <ExternalLink />
           </a>
         </Intro>
       </FirstShowWrapper>
@@ -61,16 +76,26 @@ const Showcase = props => (
       <FirstShowWrapper>
         <Img fluid={props.showThreeImage.fluid} />
         <Intro>
-          <a href="https://insights.nzherald.co.nz/article/budget-2015-where-the-budget-goes/" target="_blank" rel="noopener noreferrer">
-            Every expenditure in 2015 NZ budget<FaExternalLinkAlt />
+          <a
+            href="https://insights.nzherald.co.nz/article/budget-2015-where-the-budget-goes/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Every expenditure in 2015 NZ budget
+            <ExternalLink />
           </a>
         </Intro>
       </FirstShowWrapper>
       <FirstShowWrapper>
         <Img fluid={props.showFourImage.fluid} />
         <Intro>
-          <a href="https://insights.nzherald.co.nz/article/2017-election-forecast/" target="_blank" rel="noopener noreferrer">
-            Election prediction visualisation<FaExternalLinkAlt />
+          <a
+            href="https://insights.nzherald.co.nz/article/2017-election-forecast/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Election prediction visualisation
+            <ExternalLink />
           </a>
         </Intro>
       </FirstShowWrapper>
